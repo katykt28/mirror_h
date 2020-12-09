04a468760983e628ea95ec9fa97b1e5f9c24e82a
katykt28 <60338598+katykt28@users.noreply.github.com>
2020-12-09 14:59:10 +0900

    Add files via upload

---
+++ js/menu-btn.js
@@ -0,0 +1,11 @@
+$(function(){
+$.when(
+    open_curtain()
+    ).done(function(){
+        document.getElementById('menu-btn').style.opacity = '1';
+    });
+});
+
+// $("#effect").on('animationend webkitAnimationEnd',function(){
+//     document.getElementById('menu-btn').style.opacity = '1';
+// });
